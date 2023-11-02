3fbeebc49434385846bde54403e96d168d3a9937
Artyom Bologov <aartaka@protonmail.com>
2023-11-02 22:01:43 +0400

    Fixes and amendments.

--- libpretty.h
+++ libpretty.h
@@ -5,44 +5,42 @@
 #define __LIBPRETTY_H__
 
 #include <iso646.h>
+#include <stdbool.h>
+
+/* Missing yet useful. */
+#define eq ==
+/* Fixing inconsistent bit operations. */
+#define bitnot ~
+#define bitxor ^
 
 #define when
 #define then ?
 #define otherwise :
 
-#define for_each(var, type, length, ...)                        \
-        for (type *init##__LINE__ = (type *) (__VA_ARGS__),     \
-                     *var = init##__LINE__;                     \
-             var < (init##__LINE__ + length);                   \
+#define foreach(type, var, length, ...)                                 \
+        for (type *init ## __LINE__ = (__VA_ARGS__),                    \
+                     *var = init ## __LINE__;                           \
+             var < (init ## __LINE__ + length);                         \
              var++)
 
-// for each (i, int, 10, array)
+// foreach (int, i, 10, array)
 
-#define for_range(var, init, target)            \
-        for (int var = init;                    \
-             var != target;                     \
-             (when (init > target)              \
-              then var--                        \
+#define forrange(var, init, target)                                     \
+        for (long long int var = init;                                  \
+             var != target;                                             \
+             (when (init > target)                                      \
+              then var--                                                \
               otherwise var++))
 
-#define for_times(var, times)                   \
-        for_range(var, 0, times)
+#define fortimes(var, times)                    \
+        forrange(var, 0, times)
 
+#define with(close, var, ...)                                   \
+        for (int i_##__LINE__ = 0,                              \
+                     void *var = (void *) (__VA_ARGS__);        \
+             !i_##__LINE__;                                     \
+             (close)(var), i_##__LINE__ = 1)
 
-
-#define with(close, var, ...)                           \
-        for (int i_##__LINE__ = 0,                      \
-                     void *var = (void *) __VA_ARGS__;  \
-             i_##__LINE__ < 1;                          \
-             i_##__LINE__ ++, (close)(var))
-
-void *_new (size_t size, void *contents)
-{
-        void *allocated = malloc(size);
-        memcpy(allocated, contents, size);
-        return allocated;
-}
-
-#define new(type, ...) _new(sizeof(type), &((type){__VA_ARGS__}))
+#define new(type, ...) &((type){__VA_ARGS__})
 
 #endif /* __LIBPRETTY_H__ */
