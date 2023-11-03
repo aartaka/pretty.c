06be08766108112c15a6a9a410af3186be71b13d
Artyom Bologov <aartaka@protonmail.com>
2023-11-03 22:34:17 +0400

    libpretty: Prettier concatenation syntax.

--- libpretty.h
+++ libpretty.h
@@ -39,10 +39,10 @@
         forrange(var, 0, times)
 
 #define with(close, var, ...)                                    \
-        for (void *flag_##__LINE__ = (void *) 0,                 \
+        for (void *flag_ ## __LINE__ = (void *) 0,                 \
                      *var = (void *) (__VA_ARGS__);              \
-             !flag_##__LINE__;                                  \
-             (close)(var), flag_##__LINE__ = (void *) 1)
+             !flag_ ## __LINE__;                                   \
+             (close)(var), flag_ ## __LINE__ = (void *) 1)
 
 void *_new (size_t size, void *contents)
 {
